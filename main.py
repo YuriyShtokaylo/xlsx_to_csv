86adac474bf26d7873897365698cca922a8d9e9a
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-01-31 18:30:10 +0100

    Experimenting with headers

--- main.py
+++ main.py
@@ -4,7 +4,11 @@ import pandas as pd
 file_name = "toConv.xlsx"
 
 df = pd.read_excel(file_name)
+origin = dict(df.dtypes)
+print(origin)
 print(df)
+df.convert_dtypes()
+print(df.dtypes)
 df.to_csv("test.csv", index=False, float_format='%.2f')
 
 #pd.read_excel(file_name).to_csv("test.csv", index=False)
