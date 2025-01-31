8ec1494da938800df6c864c72ef3c6694f66aee5
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-01-31 15:24:18 +0100

    restore

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import pandas as pd
 #file_name = "convert test5.xlsx"
 #file_name = "convert TC.xlsx"
 file_name = "toConv.xlsx"
-'''
+
 df = pd.read_excel(file_name)
 print(df)
 df.to_csv("test.csv", index=False, float_format='%.2f')
-'''
-pd.read_excel(file_name).to_csv("test.csv", index=False)
+
+#pd.read_excel(file_name).to_csv("test.csv", index=False)
