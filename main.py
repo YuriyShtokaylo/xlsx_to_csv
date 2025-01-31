e198d9704ac9e8e6c72ce08a5867f263950ca833
YuriyShtokaylo <Yuriy.Shtokaylo@gmail.com>
2025-01-31 15:23:27 +0100

    Break for testing

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 import pandas as pd
-df = pd.read_excel("convert test5.xlsx")
+#file_name = "convert test5.xlsx"
+#file_name = "convert TC.xlsx"
+file_name = "toConv.xlsx"
+'''
+df = pd.read_excel(file_name)
 print(df)
 df.to_csv("test.csv", index=False, float_format='%.2f')
+'''
+pd.read_excel(file_name).to_csv("test.csv", index=False)
